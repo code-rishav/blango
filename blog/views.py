9a1ed2a916ad30f2b137af78dd3c57377e7d3151
Rishav Raj <r.08rishav@gmail.com>
2023-06-18 12:12:04 +0000

    Finish crispy forms

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from django.utils import timezone
 from blog.models import Post
-from django.shortcuts import get_object_or_404,render
+from django.shortcuts import get_object_or_404,render,redirect
+from blog.forms import CommentForm
 # Create your views here.
 def index(request):
   posts = Post.objects.filter(published_at__lte=timezone.now())
   return render(request,"blog/index.html",{"post":posts})
 
 def post_detail(request,slug):
-  post = get_object_or_404(Post,slug=slug)
-  return render(request,"blog/post-detail.html",{"post":post})
+  if request.user.is_active:
+
+    if request.method == "POST":
+      comment_form = CommentFrom(request.form)
+
+      if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.content_object = post
+        comment.creator = request.user
+        comment.save()
+        return redirect(request.path_info)
+    else:
+      comment_form = CommentForm()
+  else:
+    comment_form = None
+  return render(request,"blog/post-detail.html",{"post":post,"comment_from":comment_form})
