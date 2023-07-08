fdaeed35975e342717ecfca8924042e00d7f0807
Rishav Raj <r.08rishav@gmail.com>
2023-07-08 06:22:40 +0000

    REST/Finsih browsable API

--- blango/settings.py
+++ blango/settings.py
@@ -53,7 +53,15 @@ class Dev(Configuration):
       'crispy_bootstrap5',
       'rest_framework',
       'rest_framework.authtoken',
+      'drf_yasg',
   ]
+
+    SWAGGER_SETTINGS = {
+    "SECURITY_DEFINITIONS": {
+        "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+        "Basic": {"type": "basic"},
+        }
+    }
 
   REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
